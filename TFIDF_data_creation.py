1b5747f410323343992026fd722819f60c405932
Simon <simon.marty0@gmail.com>
2020-04-22 22:08:31 -0400

    Fix formatting

--- TFIDF_data_creation.py
+++ TFIDF_data_creation.py
@@ -8,100 +8,99 @@ from sklearn.model_selection import train_test_split
 from nltk.tokenize import word_tokenize
 
 
-
 def build_vocab_map(df):
+    words = {}
 
-	words = {}
+    # this for loop iterates through every sample in the dataframe
+    for x in range(len(df.index)):
+        wordList = df.values[x, 0].split()  # for every News Article, create a list containing every word
+        wl_no_duplicates = []
+        for word in wordList:  # This for loop deletes the duplicate words in a news article
+            if word not in wl_no_duplicates:
+                wl_no_duplicates.append(word)
 
-	#this for loop iterates through every sample in the dataframe
-	for x in range(len(df.index)):
-		wordList = df.values[x, 0].split()#for every News Article, create a list containing every word
-		wl_no_duplicates = []
-		for word in wordList: #This for loop deletes the duplicate words in a news article
-			if word not in wl_no_duplicates:
-				wl_no_duplicates.append(word)
-		
-		for item in wl_no_duplicates:#makes a dictionary with the number of news articles that contain that word
-			if item not in words:
-				words[item] =1
-			else:
-				words[item]+=1
+        for item in wl_no_duplicates:  # makes a dictionary with the number of news articles that contain that word
+            if item not in words:
+                words[item] = 1
+            else:
+                words[item] += 1
 
-	#This next part of the code deletes any words from the dictionary that are seen less in less than 100 news articles
-	delete = [key for key, val in words.items() if val<100]
-	for key in delete:
-			del words[key]
+    # This next part of the code deletes any words from the dictionary that are seen less in less than 100 news articles
+    delete = [key for key, val in words.items() if val < 100]
+    for key in delete:
+        del words[key]
 
-	return words
+    return words
 
-def split_Data(x, y): 
-	new_x = x.to_numpy()
-	new_y = y.to_numpy()
 
-	xTrain, xTest, yTrain, yTest = train_test_split(new_x, new_y, test_size =.3)
+def split_Data(x, y):
+    new_x = x.to_numpy()
+    new_y = y.to_numpy()
 
-	xTrain = pd.DataFrame(xTrain)
-	xTest = pd.DataFrame(xTest)
-	yTrain = pd.DataFrame(yTrain)
-	yTest = pd.DataFrame(yTest)
+    xTrain, xTest, yTrain, yTest = train_test_split(new_x, new_y, test_size=.3)
 
-	return xTrain, yTrain, xTest, yTest
+    xTrain = pd.DataFrame(xTrain)
+    xTest = pd.DataFrame(xTest)
+    yTrain = pd.DataFrame(yTrain)
+    yTest = pd.DataFrame(yTest)
+
+    return xTrain, yTrain, xTest, yTest
+
 
 def construct_tfidf(df, dictionary):
-	vectorizer = TfidfVectorizer(vocabulary = dictionary.keys())
-	x = vectorizer.fit_transform(df.values[:, 0])
-	df2 = pd.DataFrame(x.toarray())
+    vectorizer = TfidfVectorizer(vocabulary=dictionary.keys())
+    x = vectorizer.fit_transform(df.values[:, 0])
+    df2 = pd.DataFrame(x.toarray())
 
-	return df2
+    return df2
+
 
 def normalize_dataframe(df):
-
     x = df.to_numpy()
 
     min_max_scaler = preprocessing.MinMaxScaler()
     df_scaled = min_max_scaler.fit_transform(x)
     df = pd.DataFrame(df_scaled)
     return df
+
+
 def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--news",
+                        default="news_Stemmed.csv",
+                        help="filename of the input data")
+    parser.add_argument("--TFIDF_xTrain",
+                        default='TFIDF_xTrain.csv')
+    parser.add_argument("--TFIDF_yTrain",
+                        default='TFIDF_yTrain.csv')
+    parser.add_argument("--TFIDF_xTest",
+                        default='TFIDF_xTest.csv')
+    parser.add_argument("--TFIDF_yTest",
+                        default='TFIDF_yTest.csv')
 
-	parser = argparse.ArgumentParser()
-	parser.add_argument("--news",
-						default="news_Stemmed.csv",
-						help="filename of the input data")
-	parser.add_argument("--TFIDF_xTrain",
-						default = 'TFIDF_xTrain.csv')
-	parser.add_argument("--TFIDF_yTrain",
-						default = 'TFIDF_yTrain.csv')
-	parser.add_argument("--TFIDF_xTest",
-						default = 'TFIDF_xTest.csv')
-	parser.add_argument("--TFIDF_yTest",
-						default = 'TFIDF_yTest.csv')
+    args = parser.parse_args()
 
-	args = parser.parse_args()
+    news = pd.read_csv(args.news)
+    news = news.fillna("")
 
+    xTrain, yTrain, xTest, yTest = split_Data(news['text'], news['label'])
 
-	news = pd.read_csv(args.news)
-	news = news.fillna("")
+    dictionary = build_vocab_map(xTrain)
 
-	xTrain, yTrain, xTest, yTest = split_Data(news['text'], news['label'])
+    tfidf_dataset_train = construct_tfidf(xTrain, dictionary)
+    tfidf_dataset_test = construct_tfidf(xTest, dictionary)
+    tfidf_dataset_train = normalize_dataframe(tfidf_dataset_train)
+    tfidf_dataset_test = normalize_dataframe(tfidf_dataset_test)
 
-	dictionary = build_vocab_map(xTrain)
-
-	tfidf_dataset_train = construct_tfidf(xTrain, dictionary)
-	tfidf_dataset_test = construct_tfidf(xTest, dictionary)
-	tfidf_dataset_train = normalize_dataframe(tfidf_dataset_train)
-	tfidf_dataset_test = normalize_dataframe(tfidf_dataset_test)
-
-	print("Exporting TFIDF xTrain")
-	tfidf_dataset_train.to_csv(args.TFIDF_xTrain, index = False)
-	print("Exporting TFIDF xTest")
-	tfidf_dataset_test.to_csv(args.TFIDF_xTest, index = False)
-	print("Exporting TFIDF yTrain")
-	yTrain.to_csv(args.TFIDF_yTrain, index = False)
-	print("Exporting TFIDF yTest")
-	yTest.to_csv(args.TFIDF_yTest, index = False)
-
+    print("Exporting TFIDF xTrain")
+    tfidf_dataset_train.to_csv(args.TFIDF_xTrain, index=False)
+    print("Exporting TFIDF xTest")
+    tfidf_dataset_test.to_csv(args.TFIDF_xTest, index=False)
+    print("Exporting TFIDF yTrain")
+    yTrain.to_csv(args.TFIDF_yTrain, index=False)
+    print("Exporting TFIDF yTest")
+    yTest.to_csv(args.TFIDF_yTest, index=False)
 
 
 if __name__ == '__main__':
-	main()
+    main()
--- model.py
+++ model.py
@@ -5,56 +5,60 @@ from sklearn.naive_bayes import BernoulliNB
 from sklearn.naive_bayes import MultinomialNB
 from sklearn.linear_model import LogisticRegression
 
+
 def Multinomial_NB(xTrain, yTrain, xTest, yTest):
-	clf = MultinomialNB()
-	clf.fit(xTrain, yTrain)
-	return clf.score(xTest, yTest)
+    clf = MultinomialNB()
+    clf.fit(xTrain, yTrain)
+    return clf.score(xTest, yTest)
+
 
 def logistic_regression(xTrain, yTrain, xTest, yTest):
-	clf = LogisticRegression(solver='lbfgs')
-	clf.fit(xTrain, yTrain)
-	return clf.score(xTest, yTest)
+    clf = LogisticRegression(solver='lbfgs')
+    clf.fit(xTrain, yTrain)
+    return clf.score(xTest, yTest)
+
+
 def file_to_numpy(filename):
-	"""
-	Read an input file and convert it to numpy
-	"""
-	df = pd.read_csv(filename)
-	return df.to_numpy()
+    """
+    Read an input file and convert it to numpy
+    """
+    df = pd.read_csv(filename)
+    return df.to_numpy()
+
 
 def main():
-	parser = argparse.ArgumentParser()
-	parser.add_argument("--tfidf_xTrain",
-						help="filename for features of the tfidf training data",
-						default = "TFIDF_xTrain.csv")
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--tfidf_xTrain",
+                        help="filename for features of the tfidf training data",
+                        default="TFIDF_xTrain.csv")
 
-	parser.add_argument("--tfidf_yTrain",
-						help="filename for labels associated with tfidf training data",
-						default = "TFIDF_yTrain.csv")
+    parser.add_argument("--tfidf_yTrain",
+                        help="filename for labels associated with tfidf training data",
+                        default="TFIDF_yTrain.csv")
 
-	parser.add_argument("--tfidf_xTest",
-						help="filename for features of the tfidf test data",
-						default = "TFIDF_xTest.csv")
+    parser.add_argument("--tfidf_xTest",
+                        help="filename for features of the tfidf test data",
+                        default="TFIDF_xTest.csv")
 
-	parser.add_argument("--tfidf_yTest",
-						help="filename for labels associated with the tfidf test data",
-						default = "TFIDF_yTest.csv")
+    parser.add_argument("--tfidf_yTest",
+                        help="filename for labels associated with the tfidf test data",
+                        default="TFIDF_yTest.csv")
 
+    args = parser.parse_args()
 
+    tfidf_xTrain = file_to_numpy(args.tfidf_xTrain)
+    tfidf_yTrain = file_to_numpy(args.tfidf_yTrain)
+    tfidf_yTrain = tfidf_yTrain.ravel()
+    tfidf_xTest = file_to_numpy(args.tfidf_xTest)
+    tfidf_yTest = file_to_numpy(args.tfidf_yTest)
+    tfidf_yTest = tfidf_yTest.ravel()
 
-	args = parser.parse_args()
+    tfidf_accuracy_NB = Multinomial_NB(tfidf_xTrain, tfidf_yTrain, tfidf_xTest, tfidf_yTest)
+    print("Accuracy for tfidf dataset in Naive Bayes: ", tfidf_accuracy_NB)
 
-	tfidf_xTrain = file_to_numpy(args.tfidf_xTrain)
-	tfidf_yTrain = file_to_numpy(args.tfidf_yTrain)
-	tfidf_yTrain = tfidf_yTrain.ravel()
-	tfidf_xTest = file_to_numpy(args.tfidf_xTest)
-	tfidf_yTest = file_to_numpy(args.tfidf_yTest)
-	tfidf_yTest = tfidf_yTest.ravel()
+    tfidf_accuracy_LG = logistic_regression(tfidf_xTrain, tfidf_yTrain, tfidf_xTest, tfidf_yTest)
+    print("Accuracy for tfidf dataset in Logistic Regression: ", tfidf_accuracy_LG)
 
-	tfidf_accuracy_NB = Multinomial_NB(tfidf_xTrain, tfidf_yTrain, tfidf_xTest, tfidf_yTest)
-	print("Accuracy for tfidf dataset in Naive Bayes: ", tfidf_accuracy_NB)
-
-	tfidf_accuracy_LG = logistic_regression(tfidf_xTrain, tfidf_yTrain, tfidf_xTest, tfidf_yTest)
-	print("Accuracy for tfidf dataset in Logistic Regression: ", tfidf_accuracy_LG)
 
 if __name__ == '__main__':
-	main()
+    main()
